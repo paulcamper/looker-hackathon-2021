885a43057e8bee777f6e4e0600c55e6df0245a19
Tim Strietzel <tim@paulcamper.de>
2021-11-11 20:01:01 +0100

    new bundle

--- name-finder/src/HelloWorld.js
+++ name-finder/src/HelloWorld.js
@@ -15,7 +15,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { Space, ComponentsProvider, Span } from '@looker/components'
 import { ExtensionContext, ExtensionContext2 } from '@looker/extension-sdk-react'
-import styled from "styled-components"
 
 const extensionContext = useContext(
     ExtensionContext2
@@ -33,15 +32,6 @@ const updateRunButton = (running) => {
 const setupDashboard = (dashboard) => {
     setDashboard(dashboard)
   }
-
-export const EmbedContainer = styled.div{
-  width: 100%;
-  height: 95vh;
-  & > iframe {
-    width: 100%;
-    height: 100%;
-  }
-}
 
 const embedCtrRef = useCallback(
     (el) => {
